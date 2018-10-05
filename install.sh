b566b7caa4608ed594e53ebf7a51152c2d4c8bd6
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-10-05 12:39:54 +0900

    Update install.sh
    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ sudo apt-get install python3
 sudo apt install -y python3-pip
 #python3 -m pip install --upgrade pip
 sudo apt-get install python3-setuptools
+pip3 install setuptools --upgrade
+pip3 install wheel
 pip3 install pyusb
 pip3 install pyqt5
