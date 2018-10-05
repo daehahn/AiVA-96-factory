88adea1ca02476178b6e1f5083eaa0b57c2e64d0
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-10-05 19:12:16 +0900

    update

--- sh/info.sh
+++ sh/info.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+cd /home/inslab/workspace/XMOS/xTIMEcomposer/Community_14.3.1
+source ./SetEnv
+cd ~
+PATH=$PATH:~/workspace/XMOS/xTIMEcomposer/Community_14.3.1/bin
+
 python3 ~/workspace/AiVA-96-factory/AiVA-96-DeviceInfo.py
